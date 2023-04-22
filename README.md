c2ed3c0bd29504c859064c31bfb46dbaccada69c
Tomat <xxlennygamerxx@gmail.com>
2023-04-22 16:01:26 -0700

    Initial project.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Aomoji Vanity
+
+pretty
---
+++ src/AomojiVanity/AomojiVanity.cs
@@ -0,0 +1,7 @@
+using JetBrains.Annotations;
+using Terraria.ModLoader;
+
+namespace AomojiVanity;
+
+[UsedImplicitly(ImplicitUseKindFlags.InstantiatedWithFixedConstructorSignature)]
+public class AomojiVanity : Mod { }
