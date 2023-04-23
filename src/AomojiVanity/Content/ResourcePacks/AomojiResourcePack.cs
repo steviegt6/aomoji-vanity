72ba240658e01a02abed5db4232818b204520d74
Tomat <xxlennygamerxx@gmail.com>
2023-04-22 21:17:25 -0700

    Add our pack.

---
+++ src/AomojiVanity/Content/ResourcePacks/AomojiResourcePack.cs
@@ -0,0 +1,7 @@
+﻿using AomojiVanity.API.ResourcePacks;
+
+namespace AomojiVanity.Content.ResourcePacks; 
+
+public sealed class AomojiResourcePack : ModResourcePack {
+    protected override string RootPath => "Assets/ResourcePack";
+}
