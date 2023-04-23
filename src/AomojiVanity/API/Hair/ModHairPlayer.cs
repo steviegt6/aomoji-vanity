f27020599ae3d1e67f44368888819b2a33f40f43
Tomat <xxlennygamerxx@gmail.com>
2023-04-22 18:54:27 -0700

    Working serialization and deserialization.

--- src/AomojiVanity/API/Hair/ModHairPlayer.cs
+++ src/AomojiVanity/API/Hair/ModHairPlayer.cs
@@ -1,4 +1,5 @@
-﻿using Terraria.ModLoader;
+﻿using System.Linq;
+using Terraria.ModLoader;
 using Terraria.ModLoader.IO;
 
 namespace AomojiVanity.API.Hair;
@@ -7,21 +8,33 @@ namespace AomojiVanity.API.Hair;
 ///     Saves and loads hair data for a player.
 /// </summary>
 internal sealed class ModHairPlayer : ModPlayer {
-    private const string current_hair_id_key = nameof(CurrentHairId);
-
-    public string? CurrentHairId { get; set; }
+    private const string current_hair_id_key = "CurrentHairId";
 
     public override void SaveData(TagCompound tag) {
         base.SaveData(tag);
 
-        if (CurrentHairId != null)
-            tag.Add(current_hair_id_key, CurrentHairId);
+        var modHair = HairLoader.GetHair(Player.hair);
+
+        if (modHair != null)
+            tag.Add(current_hair_id_key, modHair.FullName);
     }
 
     public override void LoadData(TagCompound tag) {
         base.LoadData(tag);
 
-        if (tag.ContainsKey(current_hair_id_key))
-            CurrentHairId = tag.GetString(current_hair_id_key);
+        if (!tag.ContainsKey(current_hair_id_key))
+            return;
+
+        var modHairKey = tag.GetString(current_hair_id_key);
+        ModContent.SplitName(modHairKey, out var modName, out var hairName);
+
+        if (!ModLoader.TryGetMod(modName, out var modInstance))
+            return;
+
+        var modHair = modInstance.GetContent<ModHair>().FirstOrDefault(x => x.Name == hairName);
+        if (modHair == null)
+            return;
+
+        Player.hair = modHair.Type;
     }
 }
