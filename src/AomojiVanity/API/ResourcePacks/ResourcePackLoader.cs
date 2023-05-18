a09cc5a11ebb4b4e8db327b2901f18da51eeabcc
Tomat <xxlennygamerxx@gmail.com>
2023-05-17 18:28:58 -0700

    Correctly load `.rawimg` icons.

--- src/AomojiVanity/API/ResourcePacks/ResourcePackLoader.cs
+++ src/AomojiVanity/API/ResourcePacks/ResourcePackLoader.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using JetBrains.Annotations;
+using Microsoft.Xna.Framework.Graphics;
 using Mono.Cecil.Cil;
 using MonoMod.Cil;
 using Newtonsoft.Json.Linq;
+using ReLogic.Content;
 using ReLogic.Content.Sources;
+using ReLogic.Utilities;
+using Terraria;
 using Terraria.IO;
 using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
 
 namespace AomojiVanity.API.ResourcePacks;
 
@@ -39,6 +44,7 @@ public static class ResourcePackLoader {
         On_ResourcePack.HasFile += ResourcePackHasModdedFile;
         On_ResourcePack.OpenStream += ResourcePackOpenModdedStream;
         On_ResourcePack.GetContentSource += ResourcePackGetModdedContentSource;
+        On_ResourcePack.CreateIcon += ResourcePackGetRawImgIcon;
 
         On_ResourcePackList.FromJson += FromJsonAddModdedPacks;
     }
@@ -81,6 +87,23 @@ public static class ResourcePackLoader {
 
         // Modded resource packs rely on a provided content source.
         return extended.ContentSource ?? orig(self);
+    }
+
+    private static Texture2D ResourcePackGetRawImgIcon(On_ResourcePack.orig_CreateIcon orig, ResourcePack self) {
+        if (self is not ContentSourceResourcePack extended)
+            return orig(self);
+
+        // Modded resource packs rely on a provided content source.
+        if (extended.RootSource == null)
+            return orig(self);
+
+        // Fall back to the orig call since that handles a case where there is
+        // no icon.
+        if (!extended.RootSource.HasAsset("icon.rawimg"))
+            return orig(self);
+
+        using var stream = extended.RootSource.OpenStream("icon.rawimg");
+        return Main.instance.Services.Get<IAssetRepository>().CreateUntracked<Texture2D>(stream, ".rawimg").Value;
     }
 
     private static ResourcePackList FromJsonAddModdedPacks(On_ResourcePackList.orig_FromJson orig, JArray serializedState, IServiceProvider services, string searchPath) {
