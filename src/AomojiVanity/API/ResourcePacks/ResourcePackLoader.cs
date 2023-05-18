076b9f09e8c997061a4dccef38ad7b099899ed5b
Tomat <xxlennygamerxx@gmail.com>
2023-05-17 18:05:27 -0700

    Clean up `ResourcePackLoader` a tad.

--- src/AomojiVanity/API/ResourcePacks/ResourcePackLoader.cs
+++ src/AomojiVanity/API/ResourcePacks/ResourcePackLoader.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using JetBrains.Annotations;
 using Mono.Cecil.Cil;
 using MonoMod.Cil;
 using Newtonsoft.Json.Linq;
@@ -12,6 +13,7 @@ using Terraria.ModLoader;
 namespace AomojiVanity.API.ResourcePacks;
 
 public static class ResourcePackLoader {
+    [UsedImplicitly(ImplicitUseKindFlags.InstantiatedWithFixedConstructorSignature)]
     private sealed class LoaderSystem : ModSystem {
         public LoaderSystem() {
             ResourcePackLoader.Load();
@@ -43,13 +45,6 @@ public static class ResourcePackLoader {
 
     internal static void Unload() {
         modResourcePacks = null!;
-
-        /*IL_ResourcePack.ctor -= SkipPathSettingForModdedPacks;
-        On_ResourcePack.HasFile -= ResourcePackHasModdedFile;
-        On_ResourcePack.OpenStream -= ResourcePackOpenModdedStream;
-        On_ResourcePack.GetContentSource -= ResourcePackGetModdedContentSource;
-
-        On_ResourcePackList.FromJson -= FromJsonAddModdedPacks;*/
     }
 
     private static void SkipPathSettingForModdedPacks(ILContext il) {
@@ -66,27 +61,26 @@ public static class ResourcePackLoader {
     }
 
     private static bool ResourcePackHasModdedFile(On_ResourcePack.orig_HasFile orig, ResourcePack self, string fileName) {
-        if (self is not ContentSourceResourcePack extended || extended.RootSource is null)
+        if (self is not ContentSourceResourcePack extended)
             return orig(self, fileName);
 
-        // Modded resource packs rely on a provided content source.
-        return extended.RootSource.HasAsset(fileName);
+        return extended.RootSource?.HasAsset(fileName) ?? orig(self, fileName);
     }
 
     private static Stream ResourcePackOpenModdedStream(On_ResourcePack.orig_OpenStream orig, ResourcePack self, string fileName) {
-        if (self is not ContentSourceResourcePack extended || extended.RootSource is null)
+        if (self is not ContentSourceResourcePack extended)
             return orig(self, fileName);
 
         // Modded resource packs rely on a provided content source.
-        return extended.RootSource.OpenStream(fileName);
+        return extended.RootSource?.OpenStream(fileName) ?? orig(self, fileName);
     }
 
     private static IContentSource ResourcePackGetModdedContentSource(On_ResourcePack.orig_GetContentSource orig, ResourcePack self) {
-        if (self is not ContentSourceResourcePack extended || extended.ContentSource is null)
+        if (self is not ContentSourceResourcePack extended)
             return orig(self);
 
         // Modded resource packs rely on a provided content source.
-        return extended.ContentSource;
+        return extended.ContentSource ?? orig(self);
     }
 
     private static ResourcePackList FromJsonAddModdedPacks(On_ResourcePackList.orig_FromJson orig, JArray serializedState, IServiceProvider services, string searchPath) {
